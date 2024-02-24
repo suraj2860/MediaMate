7670657dd7dd379573e30278ea9d57bfbe903429
suraj2860 <surajkmr20167@gmail.com>
2024-02-24 11:26:43 +0530

    Add Comment controller and routes

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,172 @@
+import mongoose from "mongoose"
+import { Comment } from "../models/comment.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
+
+const getVideoComments = asyncHandler(async (req, res) => {
+    //TODO: get all comments for a video
+    const { videoId } = req.params
+    const { page = 1, limit = 10 } = req.query
+
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "invalid videoId");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "video not found");
+    }
+
+    const comments = await Comment.aggregatePaginate(
+        {
+            $match: { video: videoId }
+        },
+        {
+            page: parseInt(page),
+            limit: parseInt(limit)
+        }
+    );
+    
+    if(!comments) {
+        throw new ApiError(500, "DB :: something went wrong while fetching comments");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                comments,
+                "comments fetched successfully"
+            )
+        );
+});
+
+const addComment = asyncHandler(async (req, res) => {
+    // TODO: add a comment to a video
+
+    const { videoId } = req.params;
+    const { content } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "invalid videoId");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "video not found");
+    }
+
+    if(!content) {
+        throw new ApiError(400, "content is required");
+    }
+
+    const comment = await Comment.create({
+        video: videoId,
+        content,
+        owner: req.user?._id
+    });
+
+    if(!comment){
+        throw new ApiError(500, "DB :: something went wrong while adding comment");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                201,
+                comment,
+                "comment posted successfully"
+            )
+        );
+})
+
+const updateComment = asyncHandler(async (req, res) => {
+    // TODO: update a comment
+
+    const{ commentId } = req.params;
+    const { content } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(commentId)) {
+        throw new ApiError(400, "invalid commentId");
+    }
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "comment not found");
+    }
+
+    if(!content) {
+        throw new ApiError(400, "content is required");
+    }
+
+    if(comment.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(401, "you are not the owner of this comment");
+    }
+
+    comment.content = content;
+    const updatedComment = await comment.save();
+
+    if(!updatedComment) {
+        throw new ApiError(500, "something went wrong while editing comment");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                updatedComment,
+                "comment updated successfully"
+            )
+        );
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    // TODO: delete a comment
+
+    const{ commentId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(commentId)) {
+        throw new ApiError(400, "invalid commentId");
+    }
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "comment not found");
+    }
+
+    if(comment.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(401, "you are not the owner of this comment");
+    }
+
+    const deletedComment = await Comment.deleteOne({_id: commentId});
+    
+    if(!deletedComment) {
+        throw new ApiError(500, "DB :: something went wrong while deleting comment");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                deletedComment,
+                "comment removed successfully"
+            )
+        );
+})
+
+export {
+    getVideoComments,
+    addComment,
+    updateComment,
+    deleteComment
+}
