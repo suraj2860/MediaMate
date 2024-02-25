9fc1d3a5fe8b57dfd0a4f6c296514a189b12ec1f
suraj2860 <surajkmr20167@gmail.com>
2024-02-25 12:33:14 +0530

    Add healthcheck controller and routes

--- src/app.js
+++ src/app.js
@@ -21,6 +21,9 @@ import playlistRouter from './routes/playlist.routes.js';
 import commentRouter from './routes/comment.routes.js';
 import tweetRouter from './routes/tweet.routes.js';
 import likeRouter from './routes/like.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import dashboardRouter from './routes/dashboard.routes.js';
+import healthcheckRouter from './routes/healthcheck.routes.js';
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
@@ -29,6 +32,9 @@ app.use("/api/v1/playlists", playlistRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 
 export { app };
